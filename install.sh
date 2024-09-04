91b4e9aedd7d10e303c0b0dd98f8070b9f17ee6d
AlexTrinityBlock <46709835+AlexTrinityBlock@users.noreply.github.com>
2024-09-04 21:53:29 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,6 +29,6 @@
 # cd /root/Fooocus
 /root/anaconda3/bin/conda init --all
 source ~/.bashrc
-conda env create -f environment.yaml
+/root/anaconda3/bin/conda env create -f environment.yaml
 conda activate /root/anaconda3/envs/fooocus/
 pip install -r requirements_versions.txt
