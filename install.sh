fe4bfa9e018b19aedefbb2d6cc38beca34c97f7d
AlexTrinityBlock <46709835+AlexTrinityBlock@users.noreply.github.com>
2024-09-04 20:33:42 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,26 @@
+#/bin/bash
+# Install driver
+apt update -y
+apt install ubuntu-drivers-common -y
+ubuntu-drivers install --gpgpu nvidia:535-server 
+apt install nvidia-utils-535 -y
+apt install nvidia-driver-535 -y
+nvidia-smi
+
+# Install nvidia driver
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb
+dpkg -i cuda-keyring_1.1-1_all.deb -y
+apt-get update -y
+apt-get install cuda-toolkit -y
+apt-get install nvidia-gds -y
+
+# Install Anaconda
+wget https://repo.anaconda.com/archive/Anaconda3-2024.06-1-Linux-x86_64.sh
+bash Anaconda3-2024.06-1-Linux-x86_64.sh -b
+
+# Install Fooocus
+git clone https://github.com/lllyasviel/Fooocus.git
+cd Fooocus
+conda env create -f environment.yaml
+conda activate fooocus
+pip install -r requirements_versions.txt
